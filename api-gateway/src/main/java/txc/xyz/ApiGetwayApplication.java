3041c48ec30a23b8cb61782466072225c207c00b
One-Txc <1273586908@qq.com>
2017-12-07 18:03:50 +0800

    zuul filter

--- api-gateway/src/main/java/txc/xyz/ApiGetwayApplication.java
+++ api-gateway/src/main/java/txc/xyz/ApiGetwayApplication.java
@@ -4,10 +4,18 @@ package txc.xyz;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.cloud.client.SpringCloudApplication;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+import org.springframework.context.annotation.Bean;
+import txc.xyz.filter.AccessFilter;
 
 @EnableZuulProxy
 @SpringCloudApplication
 public class ApiGetwayApplication {
+    @Bean
+    public AccessFilter accessFilter() {
+        return new AccessFilter();
+    }
+
+
     public static void main(String[] args) {
         new SpringApplicationBuilder(ApiGetwayApplication.class).web(true).run(args);
     }
