a3766272b3a62ca48786285473174abd47dcca42
One-Txc <1273586908@qq.com>
2017-12-11 18:19:13 +0800

    好像没什么

--- eureka-client/src/main/java/txc/xyz/EurekaClientApplication.java
+++ eureka-client/src/main/java/txc/xyz/EurekaClientApplication.java
@@ -5,6 +5,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
+import java.util.HashMap;
+
 @SpringBootApplication
 @EnableDiscoveryClient
 public class EurekaClientApplication {
